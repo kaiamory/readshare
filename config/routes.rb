b5f7417543e28ebffbe6d15821d0638b196a8cb2
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-12-06 15:50:25 +0000

    Generate users resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :reviews
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
